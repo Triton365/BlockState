4e1f5ea70a746bf3fa67b9d6fbe8c34be4c6fb73
Triton365 <boro1026@gmail.com>
2023-03-15 01:15:52 +0900

    Added LICENSE file

--- blockstate_datapack_generator.py
+++ blockstate_datapack_generator.py
@@ -1,10 +1,9 @@
 import json, heapq, urllib.request, subprocess, os, zipfile
 
 
-VERSION = '1.19.2'
+VERSION = '1.19.4'
 PACK_FORMAT = 10
 NAMESPACE = 'blockstate'
-GENERATE_FUNCTIONS = True
 TEMP_DIRECTORY_NAME = 'BLOCKSTATE_TEMP_872be9e0a76f4da1'
 
 
@@ -348,6 +347,29 @@ with zipfile.ZipFile(f'BlockState_{VERSION}.zip','w') as main_zip:
     "description": "https://github.com/Triton365/BlockState"
   }}
 }}'''.encode())
+    with main_zip.open('LICENSE','w') as f:
+        f.write(f'''MIT License
+
+Copyright (c) 2023 Triton365
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''.encode())
     with main_zip.open(f'data/{NAMESPACE}/loot_tables/get.json','w') as f:
         fwrite = lambda x: f.write(x.encode())
         fwrite('{"type":"command","pools":[{"rolls":1,"entries":[')
