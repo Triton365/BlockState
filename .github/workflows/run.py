58d27790a1a90e6bb2d95d18bc3e4cc7e6b17dde
Triton365 <93764565+Triton365@users.noreply.github.com>
2024-02-17 00:14:58 +0900

    Update run.py

--- .github/workflows/run.py
+++ .github/workflows/run.py
@@ -2,3 +2,5 @@ import sys, zipfile
 VERSION = sys.argv[1]
 with zipfile.ZipFile(f'BlockState_{VERSION}.zip') as myzip:
     myzip.extractall()
+with open(f'VERSION.txt','f') as f:
+    f.write(f'{VERSION}')
